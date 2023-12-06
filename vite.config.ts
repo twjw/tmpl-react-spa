f2a628b7cd7560e6d9282c025dbda1222351ad4f
twjw <aaaawjwa@gmail.com>
2023-12-06 14:55:37 +0800

    修正 bootstrap vite 的 UserConfig 會溢出類型檢測

--- vite.config.ts
+++ vite.config.ts
@@ -8,44 +8,45 @@ import type { EnvType } from './.env'
 import { bootstrap } from '../toolbox-js/packages/vite'
 import { baseFontSize } from './uno.config'
 
-export default bootstrap<EnvType, 'development' | 'production'>(
-	undefined,
-	async ({ envConfig, resolveAlias }) => {
-		return defineConfig({
-			plugins: [
-				UnoCSS({
-					configFile: path.relative(__dirname, path.join(process.cwd(), 'uno.config.ts')),
-				}),
-				react(),
-				svgr(),
-				createHtmlPlugin({
-					minify: envConfig.mode !== 'development',
-					inject: {
-						data: {
-							title: envConfig.project.title,
-							htmlFontSize: `${baseFontSize}px`,
+export default ({ mode }) =>
+	bootstrap<EnvType, 'development' | 'production'>(
+		{ mode },
+		async ({ envConfig, resolveAlias }) => {
+			return defineConfig({
+				plugins: [
+					UnoCSS({
+						configFile: path.relative(__dirname, path.join(process.cwd(), 'uno.config.ts')),
+					}),
+					react(),
+					svgr(),
+					createHtmlPlugin({
+						minify: envConfig.mode !== 'development',
+						inject: {
+							data: {
+								title: envConfig.project.title,
+								htmlFontSize: `${baseFontSize}px`,
+							},
 						},
-					},
-				}),
-			],
-			resolve: {
-				alias: resolveAlias,
-			},
-			server: {
-				host: '0.0.0.0',
-				port: envConfig.server.port || 3000,
-				proxy: {
-					[envConfig.server.apiBaseUrl]: {
-						target: envConfig.server.apiUrl,
-						changeOrigin: true,
-					},
-					// [envConfig.server.wsBaseUrl]: {
-					// 	target: envConfig.server.apiUrl,
-					// 	changeOrigin: true,
-					// 	// rewrite: path => path.replace(/^\/ws/, '')
-					// }
+					}),
+				],
+				resolve: {
+					alias: resolveAlias,
 				},
-			},
-		})
-	},
-)
+				server: {
+					host: '0.0.0.0',
+					port: envConfig.server.port || 3000,
+					proxy: {
+						[envConfig.server.apiBaseUrl]: {
+							target: envConfig.server.apiUrl,
+							changeOrigin: true,
+						},
+						// [envConfig.server.wsBaseUrl]: {
+						// 	target: envConfig.server.apiUrl,
+						// 	changeOrigin: true,
+						// 	// rewrite: path => path.replace(/^\/ws/, '')
+						// }
+					},
+				},
+			})
+		},
+	)
