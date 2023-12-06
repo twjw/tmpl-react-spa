4f2e44359826bc8dcd86a3bff172330b61d8888d
twjw <aaaawjwa@gmail.com>
2023-12-06 20:41:25 +0800

    移除 vite/bootstrap 使用引入方法與插件來調用

--- vite.config.ts
+++ vite.config.ts
@@ -5,49 +5,46 @@ import svgr from 'vite-plugin-svgr'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import path from 'path'
 import type { EnvType } from './.env'
-import { bootstrap, reactPagesPlugin } from '../toolbox-js/packages/vite'
+import { autoAlias, reactPagesPlugin } from '../toolbox-js/packages/vite'
 import { baseFontSize } from './uno.config'
+import { createEnvConfig } from '../toolbox-js/packages/node'
 
-export default ({ mode }) =>
-	bootstrap<EnvType, 'development' | 'production'>(
-		{ mode },
-		async ({ envConfig, resolveAlias }) => {
-			return defineConfig({
-				plugins: [
-					UnoCSS({
-						configFile: path.relative(__dirname, path.join(process.cwd(), 'uno.config.ts')),
-					}),
-					react(),
-					svgr(),
-					createHtmlPlugin({
-						minify: envConfig.mode !== 'development',
-						inject: {
-							data: {
-								title: envConfig.project.title,
-								htmlFontSize: `${baseFontSize}px`,
-							},
-						},
-					}),
-					reactPagesPlugin(),
-				],
-				resolve: {
-					alias: resolveAlias,
-				},
-				server: {
-					host: '0.0.0.0',
-					port: envConfig.server.port || 3000,
-					proxy: {
-						[envConfig.server.apiBaseUrl]: {
-							target: envConfig.server.apiUrl,
-							changeOrigin: true,
-						},
-						// [envConfig.server.wsBaseUrl]: {
-						// 	target: envConfig.server.apiUrl,
-						// 	changeOrigin: true,
-						// 	// rewrite: path => path.replace(/^\/ws/, '')
-						// }
+export default async ({ mode }) => {
+	const envConfig = await createEnvConfig<EnvType, 'development' | 'production'>({ mode })
+
+	return defineConfig({
+		plugins: [
+			UnoCSS({
+				configFile: path.relative(__dirname, path.join(process.cwd(), 'uno.config.ts')),
+			}),
+			react(),
+			svgr(),
+			createHtmlPlugin({
+				minify: envConfig.mode !== 'development',
+				inject: {
+					data: {
+						title: envConfig.project.title,
+						htmlFontSize: `${baseFontSize}px`,
 					},
 				},
-			})
+			}),
+			autoAlias(),
+			reactPagesPlugin(),
+		],
+		server: {
+			host: '0.0.0.0',
+			port: envConfig.server.port || 3000,
+			proxy: {
+				[envConfig.server.apiBaseUrl]: {
+					target: envConfig.server.apiUrl,
+					changeOrigin: true,
+				},
+				// [envConfig.server.wsBaseUrl]: {
+				// 	target: envConfig.server.apiUrl,
+				// 	changeOrigin: true,
+				// 	// rewrite: path => path.replace(/^\/ws/, '')
+				// }
+			},
 		},
-	)
+	})
+}
