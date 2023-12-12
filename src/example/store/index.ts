b4cce2b7654c7d4f7c374bfa92aab146376cf47f
twjw <aaaawjwa@gmail.com>
2023-12-12 15:37:26 +0800

    store move path

---
+++ src/example/store/index.ts
@@ -0,0 +1,2 @@
+export * from '@/store/storage' // store/storage 請保證在第一個
+export * from '@/store/user'
--- src/store/index.ts
+++
@@ -1,2 +0,0 @@
-export * from '@/store/storage' // store/storage 請保證在第一個
-export * from '@/store/user'
